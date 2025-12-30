b4eee0330234f8b2744b117b1a68882531c9ba45
the-citto <alex.citterio@hotmail.co.uk>
2025-12-30 12:58:14 +0000

    keep `benchmark` references

--- benchmarks/__main__.py
+++ benchmarks/__main__.py
@@ -10,3 +10,11 @@ importlib.import_module("bm_lc_002_add_two_numbers")
 
 if __name__ == "__main__":
     benchmark.main()
+
+
+# @benchmark.option.complexity(benchmark.oAuto)
+# @benchmark.option.display_aggregates_only(True)
+# @benchmark.option.min_time(2.0)
+# @benchmark.option.range(64, 1024)
+# @benchmark.option.range_multiplier(2)
+# @benchmark.option.repetitions(3)
